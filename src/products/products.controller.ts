77579f751cbb4dd0bed5bc6e2068122760ae2584
Victor Emanuel Cuello <84103575+emacuello@users.noreply.github.com>
2024-07-01 20:49:41 -0300

    conexion con el microservicio de productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
+  @Get()
+  findAll() {
+    return this.productsService.findAll();
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
     console.log(id);
